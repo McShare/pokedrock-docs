94fc580b51c726ba1f0d1465a9a9415170e7382e
SuperYYT <1209897320@qq.com>
2023-08-06 16:15:39 +0800

    完善西班牙语侧导航

--- docs/.vuepress/configs/sidebar/es-ES.js
+++ docs/.vuepress/configs/sidebar/es-ES.js
@@ -11,4 +11,42 @@ export const sidebar_es_ES = [
         text: 'Hogar',
         link: '/lang/es-ES',
     },
+    {
+        text: 'Cómo utilizar',
+        children: [
+            {
+                text: 'Instalación',
+                link: '/lang/es-ES/how-to-use/installation.md',
+            },
+            {
+                text: 'Uso básico',
+                link: '/lang/es-ES/how-to-use/basic-usage.md',
+            },
+            {
+                text: 'Uso avanzado',
+                link: '/lang/es-ES/how-to-use/advanced-usage',
+            },
+        ],
+    },
+    {
+        text: 'Ilustración',
+        children: [
+            {
+                text: 'Artículo',
+                link: '/lang/es-ES/illustration/item.md',
+            },
+            {
+                text: 'Generación 1',
+                link: '/lang/es-ES/illustration/gen1.md',
+            },
+            {
+                text: 'Generación 2',
+                link: '/lang/es-ES/illustration/gen2.md',
+            },
+            {
+                text: 'Generación 3',
+                link: '/lang/es-ES/illustration/gen3.md',
+            },
+        ],
+    },
 ]
